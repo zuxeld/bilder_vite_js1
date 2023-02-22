287bd8d2e3b3a720648d4e94c05896b8dc3fcd2d
zuxeld <zuxeld@gmail.com>
2023-02-22 12:57:18 +0400

    +кастомные тени

--- vite-project1/tailwind.config.cjs
+++ vite-project1/tailwind.config.cjs
@@ -8,7 +8,11 @@ module.exports = {
     ],
   },
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'innerGood': '0 1px 2px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [],
 }
